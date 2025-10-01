4d4d4e09dca4ed62098a78e2c98fcd7d139210fb
cpitt-si <cpi@siteimprove.com>
2025-10-01 06:56:27 -0700

    Added alias for quickly making a note

--- alias.sh
+++ alias.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 setToken() {
-    #do things with parameters like $1 such as
-    ~/mfa.sh $1 $2
-    source ~/.token_file
-    echo "Your creds have been set in your env."
+  #do things with parameters like $1 such as
+  ~/mfa.sh $1 $2
+  source ~/.token_file
+  echo "Your creds have been set in your env."
 }
 alias mfa=setToken
 
-
 alias vi=vim
+
+## fucntion that takes in a title and opens a new file in vim with the format YYYY-MM-DD_meetingname.md
+makenote() {
+  if [ -z "$1" ]; then
+    echo "Please provide a note title."
+    return 1
+  fi
+  local meeting_name="$1"
+  local date_str
+  date_str=$(date +%Y-%m-%d)
+  local file_name="${date_str}_${meeting_name}.md"
+  nvim "$file_name"
+}
