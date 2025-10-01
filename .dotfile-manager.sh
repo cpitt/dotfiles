0799915374b5c3dcedd2f10153ecdfcdd7b6e31d
cpitt-si <cpi@siteimprove.com>
2025-10-01 06:52:22 -0700

    Changed original DFM script to be a function and not an alias

--- .dotfile-manager.sh
+++ .dotfile-manager.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Dotfile Manager
 # Use bare git repo to manage your dotfiles
-alias dfm="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@"
+dfm() {
+  git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
+}
 
-function dfm_init {
+dfm_init() {
   echo "Cloning dotfiles..."
-  git clone --bare https://github.com/cpitt/dotfiles $HOME/.dotfiles
+  git clone --bare https://github.com/cpitt/dotfiles "$HOME"/.dotfiles
 
   echo "Checking out config..."
-  dfm checkout 2>&1
-  if [ $? = 0 ]; then
+  if dfm checkout 2>&1; then
     echo "Checked out config."
   else
     echo "Backing up pre-existing dot files."
@@ -19,8 +20,7 @@ function dfm_init {
   fi
 
   echo "Retrying config checkout"
-  dfm checkout 2>&1
-  if [ $? = 0 ]; then
+  if dfm checkout 2>&1; then
     echo "checked out config"
   else
     echo "config checkout failed, clean your environment and try again"
@@ -33,14 +33,14 @@ function dfm_init {
   dfm submodule init
   dfm submodule update --remote
 
-  # Run install scripts 
+  # Run install scripts
   read -p "Would you like to run bootstrap scripts (y/n):" -n -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     source ./.setup-scripts/bootstrap.sh
-  else 
+  else
     exit 0
   fi
 }
 
-[ ! -d $HOME/.dotfiles ] && dfm_init
+[ ! -d "$HOME"/.dotfiles ] && dfm_init
