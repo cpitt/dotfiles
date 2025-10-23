6b72fda922b5963fef9adb76eb100803282cf19d
cpitt <me@cpitt.com>
2025-10-23 11:37:07 -0700

    Remove the install for catppuccin as it's already part of LazyVim.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,11 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-  },
+  -- Catppuccin Theme is already included in LazyVim
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
